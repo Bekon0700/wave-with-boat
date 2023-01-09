e48b324e6fa0eb15195fb053fbf45f9508d6f0cf
Emdadul Islam <43648698+Bekon0700@users.noreply.github.com>
2023-01-09 11:13:00 +0600

    done

---
+++ main.js
@@ -0,0 +1,15 @@
+// const radius = 65
+// const originX = 300
+// const originY = 300
+
+// for(let i=0; i<360; i += 5) {
+//    let [x, y] = pointCalculator(i, radius, originX, originY)
+//    console.log(x, y)
+//    $('.svg-container').append(`<polygon points="300,300 ${parseInt(x)},${parseInt(y)}" class="shape-fill" ></polygon>`)
+// }
+// function pointCalculator(angle, r, X, Y) {
+//     let x = r * Math.cos(angle) + X
+//     let y = r * Math.sin(angle) + Y
+//     return [x, y]
+// }
+// $(".container").html($(".container").html());
